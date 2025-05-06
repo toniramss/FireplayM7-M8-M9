cc44eda1173c622ec21fc6b0d332276605cfd5a0
toniramss <toni.rams.sebastia@gmail.com>
2025-05-06 17:45:42 +0200

    feat: eliminar importaciones no utilizadas en layout y firebase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
-import { ReactNode } from "react";
+//import { ReactNode } from "react";
 import Header from "../components/header";
-import Footer from "../components/footer";
+//import Footer from "../components/footer";
 import { AuthProvider } from "@/context/AuthContext";
 import { CartProvider } from "@/context/CartContext";
 import { FavoritesProvider } from "@/context/FavoritesContext";
--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp, getApps, getApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
 //import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore, collection, addDoc } from "firebase/firestore";
